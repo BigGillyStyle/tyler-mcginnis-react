3defea80210142b8094f5188e5d24b425fe7838a
Andy Pickler <andy.pickler@semanticbits.com>
2019-09-30 05:18:53 -0600

    Add state to languages

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { selectedLanguage: 'All' };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({ selectedLanguage });
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const { selectedLanguage } = this.state;
+
     return (
       <ul className="flex-center">
         {languages.map((language) => (
           <li key={language}>
-            <button type="button" className="btn-clear nav-link">
+            <button
+              type="button"
+              className="btn-clear nav-link"
+              style={language === selectedLanguage ? { color: 'red' } : null}
+              onClick={() => this.updateLanguage(language)}
+            >
               {language}
             </button>
           </li>
