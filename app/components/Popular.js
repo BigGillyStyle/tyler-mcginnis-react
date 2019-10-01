d6d9a34b178ce7eb2c9fe04e211f01d3dc22b4c8
Andy Pickler <andy.pickler@semanticbits.com>
2019-09-30 18:28:39 -0600

    Fetch repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import fetchPopularRepos from '../utils/api';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -31,21 +32,49 @@ export default class Popular extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { selectedLanguage: 'All' };
+    this.state = { selectedLanguage: 'All', repos: {}, error: null };
 
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
+  }
+
+  componentDidMount() {
+    const { selectedLanguage } = this.state;
+    this.updateLanguage(selectedLanguage);
   }
 
   updateLanguage(selectedLanguage) {
-    this.setState({ selectedLanguage });
+    const { repos: repositories } = this.state;
+    this.setState({ selectedLanguage, error: null });
+
+    if (!repositories[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) => this.setState(({ repos }) => ({ repos: { ...repos, [selectedLanguage]: data } })))
+        .catch((error) => {
+          console.warn('Error fetching repos: ', error);
+
+          this.setState({ error: 'There was an error fetching the repositories' });
+        });
+    }
+  }
+
+  isLoading() {
+    const { selectedLanguage, repos, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
   }
 
   render() {
-    const { selectedLanguage } = this.state;
+    const { selectedLanguage, repos, error } = this.state;
 
     return (
       <>
         <LanguagesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
       </>
     );
   }
