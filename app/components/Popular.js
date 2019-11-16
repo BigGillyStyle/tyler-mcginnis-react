102e0281d3c7b01ede4fe86366550c31857b75c5
Andy Pickler <andy.pickler@semanticbits.com>
2019-11-16 07:44:20 -0700

    Finished last basic React lessons

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  FaUser, FaStar, FaCodeBranch, FaExclamationTriangle,
-} from 'react-icons/fa';
-import fetchPopularRepos from '../utils/api';
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
+import { fetchPopularRepos } from '../utils/api';
+import Card from './Card';
+import Loading from './Loading';
+import Tooltip from './Tooltip';
 
 function LangaugesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
   return (
     <ul className="flex-center">
-      {languages.map((language) => (
+      {languages.map(language => (
         <li key={language}>
           <button
-            type="button"
             className="btn-clear nav-link"
             style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
             onClick={() => onUpdateLanguage(language)}
@@ -28,54 +28,40 @@ function LangaugesNav({ selected, onUpdateLanguage }) {
 
 LangaugesNav.propTypes = {
   selected: PropTypes.string.isRequired,
-  onUpdateLanguage: PropTypes.func.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
 };
 
 function ReposGrid({ repos }) {
   return (
     <ul className="grid space-around">
       {repos.map((repo, index) => {
-        const {
-          name, owner, html_url, stargazers_count, forks, open_issues,
-        } = repo;
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo;
         const { login, avatar_url } = owner;
 
         return (
-          <li key={html_url} className="repo bg-light">
-            <h4 className="header-lg center-text">
-#
-              {index + 1}
-            </h4>
-            <img className="avatar" src={avatar_url} alt={`Avatar for ${login}`} />
-            <h2 className="center-text">
-              <a className="link" href={html_url}>
-                {login}
-              </a>
-            </h2>
-            <ul className="card-list">
-              <li>
-                <FaUser color="rgb(255, 191, 116)" size={22} />
-                <a href={`https://github.com/${login}`}>{login}</a>
-              </li>
-              <li>
-                <FaStar color="rgb(255, 215, 0)" size={22} />
-                {stargazers_count.toLocaleString()}
-                {' '}
-stars
-              </li>
-              <li>
-                <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
-                {forks.toLocaleString()}
-                {' '}
-forks
-              </li>
-              <li>
-                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
-                {open_issues.toLocaleString()}
-                {' '}
-open
-              </li>
-            </ul>
+          <li key={html_url}>
+            <Card header={`#${index + 1}`} avatar={avatar_url} href={html_url} name={login}>
+              <ul className="card-list">
+                <li>
+                  <Tooltip text="Github username">
+                    <FaUser color="rgb(255, 191, 116)" size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
+                  </Tooltip>
+                </li>
+                <li>
+                  <FaStar color="rgb(255, 215, 0)" size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </Card>
           </li>
         );
       })}
@@ -84,7 +70,7 @@ open
 }
 
 ReposGrid.propTypes = {
-  repos: PropTypes.array.isRequired,
+  repos: PropTypes.array.isRequired
 };
 
 export default class Popular extends React.Component {
@@ -94,7 +80,7 @@ export default class Popular extends React.Component {
     this.state = {
       selectedLanguage: 'All',
       repos: {},
-      error: null,
+      error: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
@@ -102,32 +88,30 @@ export default class Popular extends React.Component {
   }
 
   componentDidMount() {
-    const { selectedLanguage } = this.state;
-    this.updateLanguage(selectedLanguage);
+    this.updateLanguage(this.state.selectedLanguage);
   }
 
   updateLanguage(selectedLanguage) {
     this.setState({
       selectedLanguage,
-      error: null,
+      error: null
     });
 
-    const { repos: repositories } = this.state;
-    if (!repositories[selectedLanguage]) {
+    if (!this.state.repos[selectedLanguage]) {
       fetchPopularRepos(selectedLanguage)
-        .then((data) => {
+        .then(data => {
           this.setState(({ repos }) => ({
             repos: {
               ...repos,
-              [selectedLanguage]: data,
-            },
+              [selectedLanguage]: data
+            }
           }));
         })
-        .catch((error) => {
+        .catch(() => {
           console.warn('Error fetching repos: ', error);
 
           this.setState({
-            error: 'There was an error fetching the repositories.',
+            error: 'There was an error fetching the repositories.'
           });
         });
     }
@@ -146,9 +130,9 @@ export default class Popular extends React.Component {
       <>
         <LangaugesNav selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
 
-        {this.isLoading() && <p>LOADING</p>}
+        {this.isLoading() && <Loading text="Fetching Repos" />}
 
-        {error && <p>{error}</p>}
+        {error && <p className="center-text error">{error}</p>}
 
         {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </>
